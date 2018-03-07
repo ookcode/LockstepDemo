9652b691c97af74aafbb7b6b04192beb96444e90
Vincent Yao <ookcode@gmail.com>
2018-03-07 10:10:24 +0800

    simple client

--- client/main.js
+++ client/main.js
@@ -19,131 +19,143 @@ var DIRECTION_STR = {
 // 游戏状态枚举
 var STATUS = {
 	WAIT:1,
-	START:2,
-	END:3
+	START:2
 }
 
 // 游戏对象
 var GameObject = function(id) {
-	this.id = id;
-	this.x = 0;
-	this.y = 0;
-	this.direction = DIRECTION.STOP;
-	this.speed = 100;
+	this.id = id
+	this.x = 0
+	this.y = 0
+	this.direction = DIRECTION.STOP
+	this.speed = 100
 	this.move = function (dt) {
-		dt = dt / 1000;
+		dt = dt / 1000
 		switch(this.direction) {
 			case DIRECTION.UP:
-				this.y -= this.speed * dt;
-				break;
+			{
+				this.y -= this.speed * dt
+				break
+			}
 			case DIRECTION.DOWN:
-				this.y += this.speed * dt;
-				break;
+			{
+				this.y += this.speed * dt
+				break
+			}
 			case DIRECTION.LEFT:
-				this.x -= this.speed * dt;
-				break;
+			{
+				this.x -= this.speed * dt
+				break
+			}
 			case DIRECTION.RIGHT:
-				this.x += this.speed * dt;
-				break;
+			{
+				this.x += this.speed * dt
+				break
+			}
 		}
 	}
 }
 
 $(function () {
 	// 画布
-	var context = document.getElementById("canvas").getContext("2d");
+	var context = document.getElementById("canvas").getContext("2d")
 	// 每个step的间隔ms，服务器返回
-	var stepInterval = 0;
+	var stepInterval = 0
 	// 当前step时间戳
-	var stepTime = 0;
+	var stepTime = 0
 	// 输入方向
-	var inputDirection = null;
+	var inputDirection = null
 	// 游戏状态
-	var gameStatus = STATUS.WAIT;
+	var gameStatus = STATUS.WAIT
 	// 接受指令
-	var recvCommands = new Array();
+	var recvCommands = new Array()
 	// 所有游戏对象
 	var gameObjects = {}
-	// 判断掉线，暂停游戏
-	var isNetDelay = false;
 	// 是否连接socket
-	var isConnected = false;
+	var isConnected = false
 
 	// 初始化UI显示
-	$("#content").hide();
-	$("#login").show();
-	$("#tips").hide();
+	$("#content").hide()
+	$("#login").show()
+	$("#tips").hide()
 
 	// 连接socket
-	socket = io.connect('http://120.78.185.209:3000');
-	// socket = io.connect('http://127.0.0.1:3000');
+	socket = io.connect('http://127.0.0.1:3000')
 
 	// socket连接成功
 	socket.on('open', function(json) {
-		isConnected = true;
+		isConnected = true
 		stepInterval = json.stepInterval
 		id = json.id
-		console.log("Socket连接成功：", id);
-	});
+		console.log("Socket连接成功：", id)
+	})
 
 	// 收到游戏开始事件
 	socket.on('start',function(json) {
 		// 初始化GameObject
 		for(var i = 0; i < json.player.length; ++i) {
-			var id = json.player[i];
-			gameObjects[id] = new GameObject(id);
+			var id = json.player[i]
+			gameObjects[id] = new GameObject(id)
 		}
-		gameStatus = STATUS.START;
-		stepTime = 0;
-		console.log("游戏开始");
-	});
+		gameStatus = STATUS.START
+		stepTime = 0
+		console.log("游戏开始")
+	})
 
 	// 收到加入游戏结果
 	socket.on('join',function(json) {
-		showTips(json.message);
+		showTips(json.message)
 		if(json.result) {
-			$("#login").hide();
-			$("#content").show();
+			$("#login").hide()
+			$("#content").show()
 		}
-	});
+	})
 
 	// 收到系统消息
 	socket.on('system',function(msg) {
-		showTips(msg);
-	});
+		showTips(msg)
+	})
 
 	// 收到指令
 	socket.on('message',function(json){
 		// 储存收到的指令
-		recvCommands.push(json.commands);
-		stepTime = json.step
-	});
-
-	// 收到历史指令
-	socket.on('remessage',function(json){
-		recvCommands = recvCommands.concat(json);
-	});
+		for(var i = 0; i < json.length; ++i) {
+			var command = json[i]
+			recvCommands.push(command)
+			stepTime = command[command.length - 1].step
+			console.log("**** recv " + stepTime + " ****")
+		}
+	})
 
 	// 对时
+	var avgDelay = 0
+	var avgDelayCount = 0
+	var avgDelayMax = 20
 	socket.on('timeSync',function(json) {
-		var client = json.client;
-		var server = json.server;
-		var delay = Date.now() - client; // 网络延迟
-		$("#lag").text("延迟：" + delay + "ms")
-	});
+		var client = json.client
+		var server = json.server
+		var delay = Date.now() - client // 网络延迟
+		if(avgDelayCount == avgDelayMax) {
+			$("#lag").text("延迟：" + Math.ceil(avgDelay / avgDelayMax) + " ms")
+			avgDelay = 0
+			avgDelayCount = 0
+		}
+		avgDelay = avgDelay + delay
+		avgDelayCount = avgDelayCount + 1
+	})
 
 	// 断线
 	socket.on('disconnect',function() {
 		showTips("与服务器断开连接!")
-	});
+	})
 
 	// 发送指令
 	function sendCommand() {
-		var direction = inputDirection;
+		var direction = inputDirection
 		socket.emit("message", {
 			direction: direction,
-			time:stepTime,
-		});
+			step:stepTime,
+		})
 	}
 
 	// step定时器
@@ -151,101 +163,126 @@ $(function () {
 	}
 
 	// frame定时器
-	var stepUpdateCounter = 0;
+	var stepUpdateCounter = 0
 	function update(dt) {
 		if(gameStatus == STATUS.START) {
-			stepUpdateCounter += dt;
+			// TODO: 逻辑/UI分离
+			stepUpdateCounter += dt
 			if(stepUpdateCounter >= stepInterval) {
-				stepUpdate();
-				stepUpdateCounter -= stepInterval;
+				stepUpdate()
+				stepUpdateCounter -= stepInterval
 			}
-			var scale = 1;
-			if(recvCommands.length > 2) {
-				scale = 2;
-			}
+
+			// 积攒的包过多时要加速运行
+			var scale = Math.ceil(recvCommands.length / 3)
+			if(scale > 10) scale = 10
+
+			// 执行指令
 			if(recvCommands.length > 0) {
-				var frame = recvCommands[0];
-				var ms = dt * scale;
-				if(frame.ms == undefined) frame.ms = stepInterval;
-				if(frame.ms < ms) ms = frame.ms
-				for (var i = 0; i < frame.length; i++) {
-					var command = frame[i];
-					console.log(command);
-					var obj = gameObjects[command.id];
+				var step = recvCommands[0]
+				var ms = dt * scale
+				if(step.ms == undefined) step.ms = stepInterval
+				if(step.ms < ms) ms = step.ms
+				for (var i = 0; i < step.length; i++) {
+					var command = step[i]
+					if(step.ms == stepInterval) console.log(command)
+					var obj = gameObjects[command.id]
 					if(command.direction) {
-						obj.direction = command.direction;
+						obj.direction = command.direction
 					}
 					obj.move(ms)
 				}
-				frame.ms = frame.ms - ms
-				if(frame.ms == 0) {
-					recvCommands.shift();
+				step.ms = step.ms - ms
+				if(step.ms == 0) {
+					recvCommands.shift()
 				}
 			}
-			context.clearRect(0, 0, 600, 400);
+
+			// 绘制
+			context.clearRect(0, 0, 600, 400)
 			for(var key in gameObjects) {
-				var obj = gameObjects[key];
-				context.fillStyle = "#000000";
-				context.fillRect(obj.x, obj.y, 30, 30);
+				var obj = gameObjects[key]
+				context.fillStyle = "#000000"
+				context.fillRect(obj.x, obj.y, 30, 30)
 			}
 		}
 	}
 
 	// 启动定时器
-	var lastUpdate = Date.now();
+	var lastUpdate = Date.now()
 	setInterval(function() {
-		var now = Date.now();
-		var dt = now - lastUpdate;
-		lastUpdate = now;
+		var now = Date.now()
+		var dt = now - lastUpdate
+		lastUpdate = now
 		update(dt)
 		if(isConnected == true) {
-			socket.emit("timeSync", now);
+			socket.emit("timeSync", now)
 		}
-	});
+	})
 
 	// 键盘事件
 	$('body').keydown(function(e) {
-		if(gameStatus != STATUS.START) return;
+		if(gameStatus != STATUS.START) return
 		switch(e.keyCode) {
-			case 38: inputDirection = DIRECTION.UP; break;
-			case 40: inputDirection = DIRECTION.DOWN; break;
-			case 37: inputDirection = DIRECTION.LEFT; break;
-			case 39: inputDirection = DIRECTION.RIGHT; break;
-			case 13: inputDirection = DIRECTION.STOP; break;
+			case 38: 
+			{
+				inputDirection = DIRECTION.UP 
+				break
+			}
+			case 40: 
+			{
+				inputDirection = DIRECTION.DOWN 
+				break
+			}
+			case 37: 
+			{
+				inputDirection = DIRECTION.LEFT 
+				break
+			}
+			case 39: 
+			{
+				inputDirection = DIRECTION.RIGHT 
+				break
+			}
+			case 13: 
+			{
+				inputDirection = DIRECTION.STOP 
+				break
+			}
 		}
-		sendCommand();
-	});
+		sendCommand()
+	})
 
 	// 开始游戏
 	$('#start_btn').click(function(){
-		var account = $("#account").val();
+		var account = $("#account").val()
 		if(isConnected == false) {
-			showTips("连接服务器失败！");
+			showTips("连接服务器失败！")
 		} else if(account == "") {
 			showTips("账号不能为空！")
 		} else {
-			socket.emit("join", account);
+			socket.emit("join", account)
 		}
-	});
+	})
 
 	// 断线重连
 	$('#reconnect_btn').click(function(){
-		location.reload();
-	});
-});
+		location.reload()
+	})
+})
 
 // 弹一个Tips
 function showTips(str) {
-	var width = str.length * 20 + 50;
-	var halfScreenWidth = $(window).width() / 2;
-	var halfScreenHeight = $(window).height() / 2;
-	$("#tips").stop();
-	$("#tips").show();
-	$("#tips").text(str);
-	$("#tips").css("width", width);
-	$("#tips").css("top", halfScreenHeight);
-	$("#tips").css("left", halfScreenWidth - width / 2);
-	$("#tips").animate({top:halfScreenHeight - 100});
-	$("#tips").fadeOut();
-	console.log(str);
+	var width = str.length * 20 + 50
+	var halfScreenWidth = $(window).width() / 2
+	var halfScreenHeight = $(window).height() / 2
+	$("#tips").stop()
+	$("#tips").show()
+	$("#tips").text(str)
+	$("#tips").css("width", width)
+	$("#tips").css("top", halfScreenHeight)
+	$("#tips").css("left", halfScreenWidth - width / 2)
+	$("#tips").animate({top:halfScreenHeight - 100})
+	$("#tips").fadeOut()
+	console.log(str)
 }
